43709db4791b7f60e74346a48ef84e954dd8578d
Chondan <chondansusuwan@outlook.com>
2020-09-25 19:17:38 +0700

    Updates

---
+++ precache-manifest.3ae46efe90769f66b631d4f533122b3b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "36c11d909202cb4629c3b5a6336ce5fb",
+    "url": "/Thinking-Concept-In-ReactJS/index.html"
+  },
+  {
+    "revision": "09e6692ddaecd6d02c32",
+    "url": "/Thinking-Concept-In-ReactJS/static/css/main.09bcdde2.chunk.css"
+  },
+  {
+    "revision": "7f72a97f97922bac173d",
+    "url": "/Thinking-Concept-In-ReactJS/static/js/2.90d6231e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Thinking-Concept-In-ReactJS/static/js/2.90d6231e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "09e6692ddaecd6d02c32",
+    "url": "/Thinking-Concept-In-ReactJS/static/js/main.4d90febb.chunk.js"
+  },
+  {
+    "revision": "01cb9272da40c39af16b",
+    "url": "/Thinking-Concept-In-ReactJS/static/js/runtime-main.11a30277.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/Thinking-Concept-In-ReactJS/precache-manifest.3ae46efe90769f66b631d4f533122b3b.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Thinking-Concept-In-ReactJS/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+});
